4439924e9621f6c2a25bef0d23dfc2a571016e92
Bartosz Sulkowski <mail@bsulkowski.pl>
2017-07-29 01:38:42 +0200

    Simplified the computation model

--- src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
+++ src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
@@ -10,23 +10,16 @@ abstract class Tree {
     def removeBranch(branchName: String): Option[Element]
 
     def data: Option[Value]
-    def expandReference: Option[ReferenceDefinition]
-    def expandComputation: Option[ComputationDefinition]
-    def expandFunction: Option[FunctionDefinition]
+    def definition: Option[(Path, Element)]
 
     def copy(): Element
     def delete(): Unit
   }
-  abstract class ReferenceDefinition(val target: Path) extends Element
-  abstract class ComputationDefinition(val function: Path) extends Element
-  abstract class FunctionDefinition(val argumentNames: List[String]) extends Element
 
   def root: Element
   def createNode(): Element
   def createData(value: Value): Element
-  def createReference(target: Path): Element
-  def createComputation(function: Path): Element
-  def createFunction(argumentNames: List[String]): Element
+  def createComputationNode(function: Path): Element
 
   type Value = String
 }
