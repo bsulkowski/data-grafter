f143e0cd0830abfc50599e08820361775c07d117
Bartosz Sulkowski <mail@bsulkowski.pl>
2017-11-08 00:21:29 +0100

    Adjusted the model to allow consistent support of user-defined functions
    Also minor refactorization of names

--- src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
+++ src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
@@ -3,23 +3,25 @@ package pl.bsulkowski.datagrafter
 abstract class Tree {
   abstract class Element {
     def parent: Option[Element]
-    def sourceBranchName: Option[String]
+    def parentBranch: Option[String]
 
     def branches: Map[String, Element]
-    def addBranch(branchName: String, element: Element): Option[Element]
-    def removeBranch(branchName: String): Option[Element]
+    def addBranch(name: String, element: Element): Option[Element]
+    def removeBranch(name: String): Option[Element]
 
-    def data: Option[Value]
-    def definition: Option[(Path, Element)]
-
-    def copy(): Element
-    def delete(): Unit
+    def data: Option[Data]
+    def graft: Option[Graft]
   }
 
   def root: Element
-  def createNode(): Element
-  def createData(value: Value): Element
-  def createComputationNode(function: Path): Element
+  def createNodeElement(): Element
+  def createDataElement(data: Data): Element
+  def createGraftElement(graft: Graft): Element
 
-  type Value = String
+  type Data = String
+
+  abstract class Graft
+  case class ReferenceGraft(target: Path) extends Graft
+  case class ApplicationGraft(function: Element, arguments: Element) extends Graft
+  case class FunctionGraft(arguments: Element, result: Element) extends Graft
 }
