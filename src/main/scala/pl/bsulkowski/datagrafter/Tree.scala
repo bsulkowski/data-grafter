d8380032873ff13e81818785d62ff8be179dba9b
Bartosz Sulkowski <mail@bsulkowski.pl>
2017-11-15 21:34:33 +0100

    greatly simplified the core model interface

--- src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
+++ src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
@@ -11,61 +11,48 @@ package pl.bsulkowski.datagrafter
   * Primitive data values can be stored only in leafs.
   */
 abstract class Tree {
-  /** 
-    * 
-    */
-  abstract class Element {
-    /** Parent Element of this or None in case of a root.
-      * 
-      * For elements that define a Graft it is the parent of corresponding graft Element.
-      */
-    def parent: Option[Element]
-    /** Branch name pointing to this from the parent Element or None in case of a root.
-      * 
-      * For elements that define a Graft it is the parentBranch of corresponding graft Element.
-      */
-    def parentBranch: Option[String]
-
-    /** Returns all branches of the node Element eventually resolved from this Element.
-      * 
-      * Branches for given node Element has unique names and map to direct child Elements.
-      */
-    def branches: Map[String, Element]
-    /** Attempts to add a branch for the node Element eventually resolved from this Element.
-      * 
-      * Returns None in case of success or that Element in case of failure.
-      * Parent information of that Element is updated accordingly.
-      */
-    def addBranch(name: String, that: Element): Option[Element]
-    /** Attempts to remove a branch for the node Element eventually resolved from this Element.
-      * 
-      * Returns detached Element in case of success or None in case of failure.
-      * Parent information of detached Element is updated accordingly.
-      */
-    def removeBranch(name: String): Option[Element]
-
-    /** Returns Data contained in the data Element eventually resolved from this Element.
-      */
-    def data: Option[Data]
-    /** Returns Graft used to define this graft Element.
-      */
-    def graft: Option[Graft]
-  }
 
   /** The root Element of this Tree.
     *
     * There may exist roots of other subtrees not connected to the main tree,
     * but they are not tracked explicitly.
     */
-  def root: Element
-  def createNodeElement(): Element
-  def createDataElement(data: Data): Element
-  def createGraftElement(graft: Graft): Element
+  val root: Element
 
-  type Data = String
+  /**
+    * 
+    */
+  abstract class Element {
+
+    /** Returns Handle to this or None in case of a root.
+      * 
+      * For elements that define a Graft it is the parent of corresponding graft Element.
+      */
+    def handle: Option[Handle]
+
+    /** Returns the Content of the Element eventually resolved from this Element.
+      */
+    def content: Content
+
+    /** Returns Graft used to define this graft Element.
+      */
+    def graft: Option[Graft]
+  }
+
+  case class Handle(parent: Element, branch: String)
+
+  abstract class Content
+  case class BranchesContent(branches: scala.collection.mutable.Map[String, Element]) extends Content
+  case class DataContent(data: String) extends Content
+  case class FunctionContent(function: (Element => Element)) extends Content
+  case class ErrorContent(error: String) extends Content
 
   abstract class Graft
   case class ReferenceGraft(target: Path) extends Graft
   case class ApplicationGraft(function: Element, arguments: Element) extends Graft
   case class FunctionGraft(arguments: Element, result: Element) extends Graft
+
+  def createNodeElement(): Element
+  def createDataElement(data: String): Element
+  def createGraftElement(graft: Graft): Element
 }
