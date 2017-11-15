b88ee193bc477bf84a20315e30b62da122ab4027
Bartosz Sułkowski <mail@bsulkowski.pl>
2017-11-15 02:37:08 +0100

    added some comments in model specification

--- src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
+++ src/main/scala/pl/bsulkowski/datagrafter/Tree.scala
@@ -11,18 +11,41 @@ package pl.bsulkowski.datagrafter
   * Primitive data values can be stored only in leafs.
   */
 abstract class Tree {
+  /** 
+    * 
+    */
+ 
   abstract class Element {
+    /** Parent Element of this or None in case of a root.
+      * 
+      * For elements that define a Graft it is the parent of corresponding graft Element.
+      */
     def parent: Option[Element]
+    /** Branch name pointing to this from the parent Element or None in case of a root.
+      * 
+      * For elements that define a Graft it is the parentBranch of corresponding graft Element.
+      */
     def parentBranch: Option[String]
 
+    /** Map of branch names to child Elements of this.
+      */
     def branches: Map[String, Element]
     def addBranch(name: String, element: Element): Option[Element]
     def removeBranch(name: String): Option[Element]
 
+    /** Returns Data contained in data Element.
+      */
     def data: Option[Data]
+    /** Returns Graft used to define this graft Element.
+      */
     def graft: Option[Graft]
   }
 
+  /** The root Element of this Tree.
+    *
+    * There may exist roots of other subtrees not connected to the main tree,
+    * but they are not tracked explicitly.
+    */
   def root: Element
   def createNodeElement(): Element
   def createDataElement(data: Data): Element
